1cb37bee42ed8a6c5605346e72f2b8cd767ff785
lucastorrekens <lucas.torrekens@gmail.com>
2024-09-20 16:30:51 -0300

    feat: subiendo actividad 1

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,11 @@
 # Aplicacion del servidor
+from microdot import Microdot, send_file
+
+app = Microdot()
+
+@app.get('/')
+async def index(request):
+    return send_file('/index.html')
+
+
+app.run(port = 80)
